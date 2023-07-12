cc1be53ce09f1b0a2d38fcf22c8751f4f0f94c20
hoaingodong <hoaingodong1@gmail.com>
2023-07-12 09:30:22 +0700

    feat: config vite.config.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,21 @@
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
+const defineConfig = ({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
+
+export default defineConfig;
